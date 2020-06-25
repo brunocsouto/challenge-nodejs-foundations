917ae79fe6623e51f52ac0ae2cf7e1e1b0bfe70b
Bruno Souto <bruno.csouto@gmail.com>
2020-06-24 21:34:10 -0300

    feature: transaction route

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,41 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    function sumTransactions(transactions: Transaction[]): number {
+      return transactions.reduce((total, { value }) => total + value, 0);
+    }
+
+    const incomes = this.transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+
+    const outcomes = this.transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+
+    const incomeTotal = sumTransactions(incomes);
+    const outcomeTotal = sumTransactions(outcomes);
+    const total = incomeTotal - outcomeTotal;
+
+    const balance = {
+      income: incomeTotal,
+      outcome: outcomeTotal,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
